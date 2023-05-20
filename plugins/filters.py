50f803b0cb2eb197b04c8b3b356d569655dd81fb
LOOK ME FROM EARTH <102791126+DHANANJAY4532@users.noreply.github.com>
2023-05-20 16:25:47 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -116,7 +116,7 @@ async def addfilter(client, message):
     )
 
 
-@Client.on_message(filters.command(['viewfilters', 'filters']), filters.incoming, & filters.user(ADMINS))
+@Client.on_message(filters.command(['viewfilters', 'filters']) & filters.incoming)
 async def get_all(client, message):
     
     chat_type = message.chat.type
